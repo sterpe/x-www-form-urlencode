a1927fed15c06c86d3dcb1dd26aff85d1250d674
sterpe <srterpe@gmail.com>
2015-08-17 11:30:28 -0700

    initial go at it.

---
+++ index.js
@@ -0,0 +1,18 @@
+function encode(s) {
+	return encodeURIComponent(s)
+		.replace(/\%0(?:D|d)(?=\%0(?:A|a))\%0(A|a)/g, '&')
+		.replace(/\%0(?:D|d)/g, '&')
+		.replace(/\%0(?:A|a)/g, '&')
+		.replace(/\&/g, '%0D%0A')
+		.replace(/\%20/g, '+')
+}
+
+function decode(s) {
+	return decodeURIComponent(s.replace(/\+/g, '%20'))
+		.replace(/\r\n/g, '\n')
+
+}
+module.exports = {
+	encode: encode
+	, decode: decode
+}
