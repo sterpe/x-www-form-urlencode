4057bd53bcb655e3081fdb51aec316ac58f81881
sterpe <srterpe@gmail.com>
2015-08-31 23:27:26 -0700

    CR, LF, CR+LF encoding tests.

--- __tests__/encode-test.js
+++ __tests__/encode-test.js
@@ -13,4 +13,16 @@ describe('x-www-form-urlencoded: encode'
 	
 		expect(encoded).toBe("+%25%26%2B%C2%A3%E2%82%AC")
 	})
+	it([
+		'should encode CR, LF, CR+LF all to %0D%0A'
+	].join(""), function () {
+		var xform = require('../')
+		
+		var encoded = xform.encode("\n\r");
+		expect(encoded).toBe("%0D%0A%0D%0A");
+		encoded = xform.encode("\r");
+		expect(encoded).toBe("%0D%0A");
+		encoded = xform.encode("\n");
+		expect(encoded).toBe("%0D%0A");
+	});
 })
